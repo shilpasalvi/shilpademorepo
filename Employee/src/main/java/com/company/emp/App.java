f899fd28ebda71bd7adfd373ba3247854b03a9ff
shilpasalvi <shilpa.salvi@gmail.com>
2016-01-03 18:37:34 +0530

    commit Employee Project 1st

---
+++ Employee/src/main/java/com/company/emp/App.java
@@ -0,0 +1,16 @@
+package com.company.emp;
+
+/**
+ * 
+ * @author Shilpa
+ * App.java
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       // System.out.println( "Hello World!" );
+    	Util.printMessage("Hello World!");
+    }
+}
---
+++ Employee/src/main/java/com/company/emp/Util.java
@@ -0,0 +1,19 @@
+package com.company.emp;
+
+/**
+ * 
+ * @author Shilpa
+ * Util.java
+ *
+ */
+public class Util {
+
+	/**
+	 * printMessage
+	 * @param message
+	 */
+	public static void printMessage(String message){
+		
+		System.out.println(message);
+	}
+}
